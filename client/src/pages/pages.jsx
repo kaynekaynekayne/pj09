f28cfdedf1071457eaca19e7fa1d3fd931e593a1
kaynekaynekayne <theygoon@gmail.com>
2022-09-19 22:30:46 +0900

    getting rid of container

--- client/src/pages/pages.jsx
+++ client/src/pages/pages.jsx
@@ -12,35 +12,30 @@ const Pages = () => {
     const {user}=useUserContext();
 
     return (
-        <PageStyle>
-            <Routes>
-                <Route 
-                    path="/" 
-                    element={user ? <Home /> : <Navigate to="/login" />}
-                />
-                <Route 
-                    path="/login" 
-                    element={!user ? <Login /> : <Navigate to="/" />}
-                />
-                <Route 
-                    path="/signup" 
-                    element={!user ? <Signup/> : <Navigate to="/" />}
-                />
-                <Route 
-                    path="/searched/:word"
-                    element={user ? <Searched /> : <Navigate to="/login" />}
-                />
-                <Route 
-                    path="/detail/:id" 
-                    element={user ? <Detail /> : <Navigate to="/login" />}
-                />
-            </Routes>
-        </PageStyle>
+        <Routes>
+            <Route 
+                path="/" 
+                element={user ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route 
+                path="/login" 
+                element={!user ? <Login /> : <Navigate to="/" />}
+            />
+            <Route 
+                path="/signup" 
+                element={!user ? <Signup/> : <Navigate to="/" />}
+            />
+            <Route 
+                path="/searched/:word"
+                element={user ? <Searched /> : <Navigate to="/login" />}
+            />
+            <Route 
+                path="/detail/:id" 
+                element={user ? <Detail /> : <Navigate to="/login" />}
+            />
+        </Routes>
     )
 }
 
-const PageStyle=styled.div`
-    width:100%;
-    height:100%;
-`;
+
 export default Pages;
