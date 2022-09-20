b9c6be10cd85d64f7e8a984d58ab758dc9de392d
kaynekaynekayne <theygoon@gmail.com>
2022-09-20 18:17:52 +0900

    commit

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,10 +1,10 @@
-// const { createProxyMiddleware } = require('http-proxy-middleware')
-// module.exports = function(app) {
-//     app.use(
-//     '/openApi',
-//     createProxyMiddleware({
-//         target: 'http://www.kopis.or.kr',
-//         changeOrigin: true,
-//     })
-//     )
-// } 
+const { createProxyMiddleware } = require('http-proxy-middleware')
+module.exports = function(app) {
+    app.use(
+    '/openApi',
+    createProxyMiddleware({
+        target: 'http://www.kopis.or.kr',
+        changeOrigin: true,
+    })
+    )
+} 
